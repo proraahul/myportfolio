65d25e196f579492bff28583f3e3289c202d0bbe
Rahul saini <rs7965498@gmail.com>
2024-06-01 15:43:32 +0530

    add resume

--- src/data/constants.js
+++ src/data/constants.js
@@ -22,7 +22,7 @@ export const Bio = {
     "I am a motivated and versatile individual, always eager to take on new challenges. With a passion for learning I am dedicated to delivering high-quality results. With a positive attitude and a growth mindset, I am ready to make a meaningful contribution and achieve great things.",
   github: "https://github.com/proraahul",
   resume:
-    "https://drive.google.com/file/d/1ffZrcMcn8UatXGIaautbbqpV7ADNaETA/view?usp=sharing",
+    "https://drive.google.com/file/d/1PNY7DV9YC3jtqqU8WJQMVwLw_1p6SqGb/view?usp=sharing",
   linkedin: "https://www.linkedin.com/in/rahulsaini3113/",
   twitter: "https://twitter.com/RishavChanda",
   insta: "https://www.instagram.com/raahulsaini_3113/",
